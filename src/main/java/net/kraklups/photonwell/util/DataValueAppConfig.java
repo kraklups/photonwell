3b193931b730a815bc6389b83efe5e2163c28dab
nexus7 <kraklups@gmail.com>
2015-04-27 19:13:56 +0200

    Changing from jar to war (App, pom.xml, ...)

--- src/main/java/net/kraklups/photonwell/util/DataValueAppConfig.java
+++ src/main/java/net/kraklups/photonwell/util/DataValueAppConfig.java
@@ -4,13 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource({"classpath:spring-config.xml"})
-public class DataValueAppConfig {
+public class DataValueAppConfig extends SpringBootServletInitializer {
 
 	static final Logger logger = LoggerFactory.getLogger(DataValueAppConfig.class);
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DataValueAppConfig.class);
+	}        
 	
 	public static void main(String[] args) {
 		
